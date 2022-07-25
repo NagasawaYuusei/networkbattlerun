9f158fc5aef38bd7624e2c8791dacd199a8c0282
sakezuma <sakezuma@gmail.com>
2022-07-25 19:55:48 +0900

    Itembase（基底クラス）を作った。

---
+++ Assets/Nitsuma/Script/TestItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestItem : Itembase
+{
+    protected override void GetItem()
+    {
+        Debug.Log("TestItemƒQƒbƒg");
+    } 
+}
