16b12191d92ec18e5d3fcbdbe68677484ea11a62
MutoAyumu <mutoayum0610@gmail.com>
2022-10-17 12:27:24 +0900

    [update]アイテムを取得する部分

--- Assets/Nitsuma/Script/GetItem.cs
+++ Assets/Nitsuma/Script/GetItem.cs
@@ -7,21 +7,40 @@ public class GetItem : MonoBehaviour
     List<Itembase> _items = new List<Itembase>();
     readonly SpeedUPItem _speedUPItem = new SpeedUPItem();
     readonly TestItem _testItem = new TestItem();
+
+    [SerializeField] string _itemTag = "";
+    Itembase _currentItem;
+
     private void Awake()
     {
         _items.Add(_speedUPItem);
         _items.Add(_testItem);
     }
+
+    private void Update()
+    {
+        if (!_currentItem) return;
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            _currentItem.Use();
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (!_currentItem.CompareTag(_itemTag)) return;
+
+        //ƒAƒCƒeƒ€‚ğ‚Á‚Ä‚¢‚½‚çæ“¾‚µ‚È‚¢
+        if (_currentItem) return;
+
         RandamItemGet();
-        Destroy(gameObject);
     }
 
     void RandamItemGet()
     {
         var num = Random.Range(0, _items.Count);
 
-        _items[num].Use();
+        _currentItem = _items[num];
     }
 }
